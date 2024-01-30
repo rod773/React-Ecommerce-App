33faab8659b6e9078ba93757029dd956a6ab6fe5
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-30 10:20:29 +0100

    update pacakages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer'
-import Home from './components/Home'
-import About from './components/About'
-import Product from './components/Product'
-import Contact from './components/Contact'
-import { Redirect, Route, Switch } from 'react-router-dom';
-import ProductDetail from './components/ProductDetail';
-import Cart from './components/Cart';
-import Checkout from './components/Checkout'
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
+import Product from "./components/Product";
+import Contact from "./components/Contact";
+import { Route, Routes } from "react-router-dom";
+import ProductDetail from "./components/ProductDetail";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
 
 function App() {
   return (
     <>
-      <Header/>
-      <Switch>
+      <Header />
+      <Routes>
         <Route exact path="/" component={Home} />
         <Route exact path="/products" component={Product} />
         <Route exact path="/products/:id" component={ProductDetail} />
@@ -22,9 +22,9 @@ function App() {
         <Route exact path="/checkout" component={Checkout} />
         <Route exact path="/about" component={About} />
         <Route exact path="/contact" component={Contact} />
-        <Redirect to="/" />
-      </Switch>
-      <Footer/>
+        <Route exact path="/*" element={<Home />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
-import 'font-awesome/css/font-awesome.min.css'
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import store from './redux/store'
-import {Provider} from 'react-redux'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "font-awesome/css/font-awesome.min.css";
+import "./index.css";
+import App from "./App";
+import { createRoot } from "react-dom/client";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
-ReactDOM.render(
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+
+root.render(
   <BrowserRouter>
-  <Provider store={store}>
-    <App />
+    <Provider store={store}>
+      <App />
     </Provider>
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
